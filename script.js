545ca789a2e2d50acb30401ff210e455621cc67d
Pramod Dange <123957464+Pramoddange@users.noreply.github.com>
2024-06-03 12:44:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let body=document.getElementById('container');
+body.innerHtml=` 
+<div class="box">
+<h2 id="verification_heading"></h2>
+<h6 id=" verification_subtext "></h6>
+<input></input>
+</div>
+`
